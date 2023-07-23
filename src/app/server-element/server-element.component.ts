62aeb8437276c8e9c9858618ef9cf31762a73596
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-07-23 11:33:14 +0330

    Assigning an Alias to custom properties

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -6,6 +6,6 @@ import { Component , Input } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent {
-  @Input() element : {type :string , name: string , content : string }
+  @Input('srvElement') element : {type :string , name: string , content : string }
 
 }
