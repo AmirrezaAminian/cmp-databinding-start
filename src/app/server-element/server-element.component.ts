89e763e83a8e45f58a298b6cbc8fd1dd97929bdf
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-07-23 14:31:59 +0330

    Adding Encapsulation

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,10 @@
-import { Component , Input } from '@angular/core';
+import { Component , Input , ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'] ,
+  encapsulation : ViewEncapsulation.Emulated   //None , Native 
 })
 export class ServerElementComponent {
   @Input('srvElement') element : {type :string , name: string , content : string }
